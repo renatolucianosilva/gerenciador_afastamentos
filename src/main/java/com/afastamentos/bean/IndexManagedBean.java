2756d7c05475b42ec0de175b7d290ead916fe9c3
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-02-18 17:04:08 -0300

    RENAME CLASS

---
+++ src/main/java/com/afastamentos/bean/IndexManagedBean.java
@@ -0,0 +1,35 @@
+package com.afastamentos.bean;
+
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+import org.primefaces.event.SelectEvent;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+@Named
+@ViewScoped
+@Getter
+@Setter
+
+public class IndexManagedBean {
+    private LocalDate date;
+    private String dataSelecionada;
+
+    public void onDateSelect(SelectEvent<LocalDate> event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Data Selecionada", event.getObject().format(formatter)));
+
+         dataSelecionada = formatter.format(event.getObject());
+    }
+
+
+
+}
